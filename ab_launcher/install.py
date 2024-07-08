d21e7a2b23a7c1909b9d64a1430879d1f5e052b3
Marin Visscher <m.r.visscher@science.leidenuniv.nl>
2024-07-08 15:34:41 +0200

    Better notifying of download and installation

--- ab_launcher/install.py
+++ ab_launcher/install.py
@@ -4,15 +4,17 @@ import tarfile
 import threading
 import subprocess
 import urllib.request
-from tkinter import Tk, ttk
+from tkinter import Tk, ttk, IntVar
 
 from ab_launcher import LOCAL, INSTALL
 from ab_launcher.main import ENV_DIR, PY_DIR, AB_DIR
 
 
 class InstallationNotifier(Tk):
-    def __init__(self):
+    def __init__(self, install_fn):
         super().__init__()
+
+        self.install_fn = install_fn
 
         # Set window title and size
         self.title("Installation")
@@ -29,67 +31,130 @@ class InstallationNotifier(Tk):
         self.install_button = ttk.Button(self, text="Install", command=self.confirmed)
         self.install_button.pack(pady=5)
 
+        # Create (but don't pack) the progress bar
+        self.progress = IntVar()
+        self.progress_bar = ttk.Progressbar(self, maximum=100, variable=self.progress)
+
     def confirmed(self):
         self.install_button.destroy()
-        threading.Thread(target=self.in_thread).start()
+        self.progress_bar.pack(padx=10, fill="x", expand=1)
+        threading.Thread(target=self.install_fn).start()
 
-    def in_thread(self):
-        dl = download_base_env(self.label)
-        extract_base_env(self.label, dl)
-        install_spec_env(self.label)
+    def notify(self, message):
+        print(message)
+        self.label.config(text=message)
 
-        # create installed file as a flag that installation was succesful
+    def undefined_progress(self):
+        setattr(self.progress_bar, "undefined_progress", True)
+        self.progress_bar.config(mode="indeterminate")
+        self.progress_bar.start(5)
+        self.progress.set(0)
+
+    def set_progress(self, progress: float):
+        if getattr(self.progress_bar, "undefined_progress", False):
+            setattr(self.progress_bar, "undefined_progress", False)
+            self.progress_bar.config(mode="determinate")
+            self.progress_bar.stop()
+        self.progress.set(int(progress))
+
+
+class Installer:
+
+    def __init__(self):
+        self.notifier = InstallationNotifier(self.threaded_install)
+
+    def start(self):
+        return self.notifier.mainloop()
+
+    def threaded_install(self):
+        dl = self.download_base_env()
+        self.extract_base_env(dl)
+
+        dl = self.download_env_spec()
+        self.install_spec_env(dl)
+
+        # create installed file as a flag that installation was successful
         with open(os.path.join(AB_DIR, "installed"), "w") as file:
             file.writelines(["Installed"])
 
-        self.after(1000, self.destroy)
+        self.notifier.after(1000, self.notifier.destroy)
+
+    def download_base_env(self):
+
+        def report_hook(block_num, block_size, total_size):
+            if total_size == 0:
+                return
+            percent = (block_num * block_size) / total_size * 100
+            self.notifier.set_progress(percent)
+            print(percent)
+
+        self.notifier.set_progress(0)
+        self.notifier.notify("Downloading base environment...")
+
+        if sys.platform == "win32":
+            base_env_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/win-environment.tar.gz"
+        elif sys.platform == "darwin":
+            base_env_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/mac-environment.tar.gz"
+        else:
+            raise OSError
+        path, _ = urllib.request.urlretrieve(base_env_url, reporthook=report_hook)
+        return path
+
+    def extract_base_env(self, download):
+        self.notifier.notify("Extracting base environment tar...")
+        self.notifier.undefined_progress()
+
+        with tarfile.open(download) as file:
+            file.extractall(ENV_DIR)
+
+    def download_env_spec(self):
+        print("Downloading environment specification...")
+        if sys.platform == "win32":
+            env_spec_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/win-environment_spec.txt"
+        elif sys.platform == "darwin":
+            env_spec_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/mac-environment_spec.txt"
+        else:
+            raise OSError
+        path, _ = urllib.request.urlretrieve(env_spec_url)
+
+        # rename to a .txt file, because that's what conda supports
+        txt_path = path + ".txt"
+        os.rename(path, txt_path)
+
+        return txt_path
+
+    def install_spec_env(self, path):
+        installer = subprocess.Popen(
+            [PY_DIR, INSTALL, path],
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+        stream = ""
+        line = ""
+        anchors = ["Downloading environment specification",
+                   "Collecting package metadata",
+                   "Solving environment",
+                   "Preparing transaction",
+                   "Verifying transaction",
+                   "Executing transaction",
+                   ]
+
+        while installer.poll() is None:
+            char = installer.stdout.read(1)
+            line += char
+            stream += char
+
+            if char == '' and installer.poll() is not None:
+                break
+            if char in ('\n', '\r'):
+                if line:
+                    print(line)
+                    line = ''
+            if anchors and anchors[0] in stream:
+                self.notifier.notify(anchors.pop(0))
 
 
 def install():
-    InstallationNotifier().mainloop()
+    Installer().start()
 
-    return
-
-
-def download_base_env(label):
-    print("Downloading base environment...")
-    label.config(text="Downloading base environment...")
-
-    if sys.platform == "win32":
-        base_env_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/win-environment.tar.gz"
-        path, _ = urllib.request.urlretrieve(base_env_url)
-        return path
-    elif sys.platform == "darwin":
-        return os.path.join(LOCAL, "download", "mac-environment.tar.gz")
-    else:
-        raise OSError
-
-
-def extract_base_env(label, download):
-    print("Extracting base environment tar...")
-    label.config(text="Extracting base environment tar...")
-
-    with tarfile.open(download) as file:
-        file.extractall(ENV_DIR)
-
-
-def install_spec_env(label):
-    installer = subprocess.Popen(
-        [PY_DIR, INSTALL],
-        stdout=subprocess.PIPE,
-        text=True
-    )
-
-    line = ''
-    while installer.poll() is None:
-        char = installer.stdout.read(1)
-        if char == '' and installer.poll() is not None:
-            break
-        if char in ('\n', '\r'):
-            if line:
-                label.config(text=line)
-                print(line)
-                line = ''
-        else:
-            line += char
-            label.config(text=line)
--- ab_launcher/runners/install-runner.py
+++ ab_launcher/runners/install-runner.py
@@ -1,25 +1,6 @@
-import os
 import sys
-import urllib.request
-from pathlib import Path
-
 from conda.cli import main
-
-ROOT = Path(__file__).parent.parent
-
-
-def download_env_spec():
-    print("Downloading environment specification...")
-    if sys.platform == "win32":
-        env_spec_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/win-environment_spec.txt"
-    elif sys.platform == "darwin":
-        env_spec_url = "https://github.com/mrvisscher/AB-launcher/raw/ac3dde812d7faac173e65972fefb29ae7b9e476d/ab_launcher/download/mac-environment_spec.txt"
-    else:
-        raise OSError
-    path, _ = urllib.request.urlretrieve(env_spec_url, "env_spec.txt")
-    return path
 
 
 if __name__ == "__main__":
-    spec_file = download_env_spec()
-    main("env", "update", "--prune", "--file", spec_file, "--name", "base", "--quiet")
+    main("env", "update", "--prune", "--file", sys.argv[1], "--name", "base", "--quiet")
