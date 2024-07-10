63cbd652d1ed5136a05cf490382ebe8e62da817a
Marin <m.r.visscher@science.leidenuniv.nl>
2024-07-10 16:11:33 +0200

    added `--yes` to install runner

--- ab_launcher/runners/install-runner.py
+++ ab_launcher/runners/install-runner.py
@@ -3,5 +3,5 @@ from conda.cli import main
 
 
 if __name__ == "__main__":
-    result = main("create", "-p", sys.argv[1], "--file", sys.argv[2], "--verbose", "--verbose")
+    result = main("create", "-p", sys.argv[1], "--file", sys.argv[2], "--verbose", "--verbose", "--yes")
     sys.exit(result)
